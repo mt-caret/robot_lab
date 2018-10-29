505796582fcd782e37fdf72e125eaaa2f50cc95e
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2018-10-29 12:54:08 +0900

    refactor

--- src/robot_lab/main.py
+++ src/robot_lab/main.py
@@ -4,6 +4,24 @@ import json
 import rospy
 from std_msgs.msg import String
 
+from enum import Enum
+
+Motor = Enum('Motor', 'Right Left')
+
+def generate_message(motor, speed):
+    scaling_constant = 100.0
+    clamped_speed = max(-1.0, min(1.0, speed))
+    scaled_speed = clamped_speed * scaling_constant
+    direction = 'clockwise' if (motor == Motor.Right) == (speed >= 0.0) else 'counter-clockwise'
+    message =
+        { 'command': 'run'
+        , 'parameters':
+            { 'direction': direction
+            , 'speed': scaled_speed
+            }
+        }
+    return json.dumps(message)
+
 def main():
     motor_right = rospy.Publisher('/motor/right', String, queue_size=1)
     motor_left = rospy.Publisher('/motor/left', String, queue_size=1)
@@ -11,24 +29,9 @@ def main():
     rospy.init_node('main_control')
     rate = rospy.Rate(10)
 
-    move_clockwise_message = json.dumps(
-            { 'command': 'run'
-            , 'parameters':
-                { 'direction': 'clockwise'
-                , 'speed': 100.0
-                }
-            })
-    move_counterclockwise_message = json.dumps(
-            { 'command': 'run'
-            , 'parameters':
-                { 'direction': 'counter-clockwise'
-                , 'speed': 100.0
-                }
-            })
-
     while not rospy.is_shutdown():
-        motor_left.publish(move_clockwise_message)
-        motor_right.publish(move_counterclockwise_message)
+        motor_left.publish(generate_message(Motor.Left, 1.0))
+        motor_right.publish(generate_message(Motor.Right, 1.0))
         rate.sleep()
 
 if __name__ == '__main__':
